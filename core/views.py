adccae2de96d093da708edfdd856177479ac57c5
afeez1131 <lawalafeez052@gmail.com>
2022-05-08 17:18:36 +0100

    cleaned core views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from taggit.models import Tag
@@ -36,9 +36,6 @@ def search_for_cause(request):
                                                     'query': query, })
     else:
         return render(request, 'search_page.html', {})
-
-
-# def search_for_cause_result(request):
 
 
 def user_cause(request, username):
