b7abb5bc57f1d5801adfb9b66365f757a35e0823
afeez1131 <lawalafeez052@gmail.com>
2022-05-16 15:25:52 +0100

    fixed database env error

--- VoteForCause/settings.py
+++ VoteForCause/settings.py
@@ -98,9 +98,9 @@ WSGI_APPLICATION = 'VoteForCause.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         "HOST": 'localhost',
         # "PORT": env("DB_PORT", "5432")
     }
@@ -198,7 +198,7 @@ SUMMERNOTE_CONFIG = {
 # production settings
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = "DENY"
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 3600
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True  # new
